3c8d456abb3f5086349bb2befb8797e9ba49d52e
Max Kyrchanov <max.kyrchanov@gmail.com>
2025-07-03 20:16:39 +0500

    complete config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,22 @@
+/// <reference types='vitest' />
+/// <reference types='vite/client' />
+ 
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { name } from './package.json'
+import tailwindcss from '@tailwindcss/vite' 
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(), 
+    tailwindcss(), 
+  ],
+  base: `/${name}/`,
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: ['./src/setupTests.ts'],
+  },
 })
+
