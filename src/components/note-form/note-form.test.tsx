479877a43ed0b3468603250b311344696fc48634
Max Kyrchanov <max.kyrchanov@gmail.com>
2025-07-07 21:39:51 +0500

    feat: add notes app

--- src/components/note-form/note-form.test.tsx
+++ src/components/note-form/note-form.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen, waitFor } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import NoteForm from './note-form'
 import { vi } from 'vitest'
